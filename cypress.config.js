d1644ae5c6e0150c40161ea4c2c67a98c239d71c
Luiz Fernando <luizfgomes42@outlook.com>
2025-02-27 15:35:11 -0300

    Create cypress project

--- cypress.config.js
+++ cypress.config.js
@@ -3,11 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    baseUrl: 'http://localhost:3000', // Adjust based on your application
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-  },
+  e2e: {},
 })
+
